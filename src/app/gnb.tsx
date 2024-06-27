2e7e2c7b62eaf825ce3619c1413a7d29e097450a
houndhollis <houndhollis9797@gmail.com>
2024-06-27 10:05:57 +0900

    사이드 메뉴 Gnb 불필요한 코드 수정

--- src/app/gnb.tsx
+++ src/app/gnb.tsx
@@ -1,29 +1,32 @@
 'use client';
 
-import { ChildrenRoute, gnbRootList, isParentRoute, ParentRoute, ROUTE, routes } from "@/routes";
+import { ChildrenRoute, gnbRootList, isParentRoute, ParentRoute, ROUTE, routes, ROUTE_PATH } from "@/routes";
 import Link from "next/link";
 import classNames from "classnames";
 import { useParams } from "next/navigation";
 
-const ParentGnbItem = ({name, link, children} : ParentRoute) => {
+const ParentGnbItem = ({route : {name, link, children}, currentPath} : {route: ParentRoute; currentPath: ROUTE_PATH}) => {
+
+  const open = children.includes(currentPath);
+
   return (
-    <li className={classNames('parent', `items-${children.length}`)}>
+    <li className={classNames('parent', `items-${children.length}`,{open})}>
       <Link href={link}>
         {name} 
       </Link>
       <ul className="subRoutes">
         {children.map(r => {
           const route = routes[r];
-          return <GnbItem {...route} key={route.key}/>
+          return <GnbItem route={route} currentPath={currentPath} key={route.key}/>
         })}
       </ul>
     </li>
   )
 }
 
-const ChildrenGnbItem = ({link, name} : ChildrenRoute) => {
+const ChildrenGnbItem = ({route :{name, link}, currentPath} : {route : ChildrenRoute; currentPath: ROUTE_PATH}) => {
   return (
-    <li>
+    <li className={classNames({active : link === currentPath})}>
       <Link href={link}>
         {name} 
       </Link>
@@ -31,23 +34,26 @@ const ChildrenGnbItem = ({link, name} : ChildrenRoute) => {
   )
 }
 
-const GnbItem = (route : ROUTE) => {
-  return isParentRoute(route) ? <ParentGnbItem {...route}/> : <ChildrenGnbItem {...route}/>
+const GnbItem = ({route, currentPath} : {route :ROUTE; currentPath: ROUTE_PATH}) => {
+  return isParentRoute(route) ? <ParentGnbItem route={route} currentPath={currentPath}/> : <ChildrenGnbItem route={route} currentPath={currentPath}/>
 }
 
 
 const GnbPage = () => {
 
   const {item = []} = useParams();
-  const currentPath = ['',...item].join('/');
-
+  const currentPath = ['',...item].join('/') as ROUTE_PATH
 
   return (
     <aside>
-      <h1>UI 요소 모음</h1>
+      <h1>
+        <Link href='/'>
+          UI 요소 모음
+        </Link>
+      </h1>
       <ul className="mainRoutes">
         {gnbRootList.map((r) => (
-          <GnbItem {...r} key={r.key}/>
+          <GnbItem route={r} currentPath={currentPath} key={r.key}/>
         ))}
       </ul>
     </aside>
